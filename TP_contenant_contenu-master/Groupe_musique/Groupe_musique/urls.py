a7eb082dacd838956eaead2c07e6d8bcd16f6474
Lucas-hrl <lucas.herchuel@uha.fr>
2025-05-10 15:39:45 +0200

    redirection vers index

--- TP_contenant_contenu-master/Groupe_musique/Groupe_musique/urls.py
+++ TP_contenant_contenu-master/Groupe_musique/Groupe_musique/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic.base import RedirectView
+
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/artistes/index/', permanent=False)),
     path('admin/', admin.site.urls),
     path('artistes/',include("artistes.urls")),
 ]
